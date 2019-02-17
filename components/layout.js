71b84c1cfb027f1935c83cf0104ce46650ec2926
Beldur <alexluecking@gmail.com>
2019-02-17 20:59:21 +0100

    fix: broken icons

--- components/layout.js
+++ components/layout.js
@@ -12,8 +12,8 @@ export default ({ children, title = 'Home' }) => (
       <link rel="stylesheet" href="static/css/main.css" />
       <link
         rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-        integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
         crossorigin="anonymous"
       />
     </Head>
